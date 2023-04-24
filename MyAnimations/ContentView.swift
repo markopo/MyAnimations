bbef1ca25f297b3e52a0202fdcc4d49253addaa8
markopedu <69790644+markopedu@users.noreply.github.com>
2023-04-24 18:33:54 +0200

    first commit

--- MyAnimations/ContentView.swift
+++ MyAnimations/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Button("Tap me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            .animation(.default, value: animationAmount)
+            .blur(radius: (animationAmount - 1) * 1.5)
         }
         .padding()
     }
