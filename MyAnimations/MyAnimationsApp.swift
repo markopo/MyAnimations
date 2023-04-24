62cc4359e47e9bc0322a7b045aafa7f461039ef5
markopedu <69790644+markopedu@users.noreply.github.com>
2023-04-24 18:25:34 +0200

    Initial Commit

---
+++ MyAnimations/MyAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAnimationsApp.swift
+//  MyAnimations
+//
+//  Created by Marko Poikkimäki on 2023-04-24.
+//
+
+import SwiftUI
+
+@main
+struct MyAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
